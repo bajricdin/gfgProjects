87de50302f18d1be1a4b63169a273a3f949c8d36
bajricdin <din.bajric@stu.ibu.edu.ba>
2025-01-06 23:33:24 +0100

    Added few functions to the 2048 game.

--- 2048/logic.py
+++ 2048/logic.py
@@ -47,5 +47,71 @@ def get_current_state(mat):
     return 'LOST'
 
 
-print(start_game())
+def compress(mat):
+    changed = False
+
+    new_mat = []
+    for i in range(4):
+        new_mat.append([0] * 4)
+
+    for i in range(4):
+        pos = 0
+
+        for j in range(4):
+            if mat[i][j] != 0:
+                new_mat[i][pos] = mat[i][j]
+                if pos != j:
+                    changed = True
+                pos += 1
+
+    return new_mat, changed
+
+def merge(mat):
+    changed = False
+
+    for i in range(4):
+        for j in range(3):
+            if (mat[i][j] == mat[i][j+1] and mat[i][j] != 0):
+                mat[i][j] = mat[i][j] * 2
+                mat[i][j+1] = 0
+                changed = True
+
+    return mat, changed
+
+def reverse(mat):
+    new_mat = []
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[i][3-j])
+    return new_mat
+
+def transpose(mat):
+    new_mat = []
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[j][i])
+    return new_mat
+
+def move_left(grid):
+    new_grid, changed1 = compress(grid)
+
+    new_grid, changed2 = merge(new_grid)
+
+    changed = changed1 or changed2
+
+    new_grid = compress(new_grid)
+
+    return new_grid, changed
+
+
+new_mat = [[2,2,0,0],[2,2,0,0],[2,2,0,0],[2,2,0,0]]
+# print(reverse(new_mat))
+print(new_mat)
+print(transpose(new_mat))
+# print(new_mat)
+# print(merge(new_mat))
+print(compress(start_game()))
+
 
